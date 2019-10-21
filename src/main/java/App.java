fade736d86c6dfa302f1e43bbb9bc24d18449436
atembamanu <atembaemm07@gmail.com>
2019-10-21 10:16:31 +0300

    add app layout handlers bar file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,15 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/", (req, res)-> new ModelAndView(new HashMap<>(), "index.hbs"), new HandlebarsTemplateEngine());
+    }
 }
